b5ca812d28857c5d5ff0843efd68ca2d4fd14c9d
JClerc <jices25@gmail.com>
2018-01-22 17:53:55 +0100

    Improve help and change tabs from cli

--- app/config/index.js
+++ app/config/index.js
@@ -13,26 +13,50 @@ const args = process.argv.slice(2).map(arg => arg.replace(/-/g, ''));
 
 // no args -> print help
 if (args.length === 0 || args.some(arg => arg === 'h' || arg === 'help')) {
-  console.log(`\n${colors.blue('HELP')} Please append a configuration name to start:`);
-  Object.entries(extensions).forEach(([name, ext]) => {
-    console.log(`${colors.gray('-')} node app ${colors.white(name)} ${colors.gray(`# ${ext.description}`)}`);
-  });
+  // print "node app <cmd> # <desc>"
+  const usage = (cmd, desc) => {
+    console.log(`${colors.gray('-')} node app ${colors.white(cmd)} ${colors.gray(`# ${desc}`)}`);
+  };
+
+  // usage
+  console.log(`\n${colors.blue('USAGE')} node app [configuration ...] [tabs]`);
+
+  // help
+  console.log(`\n${colors.blue('HELP')} Please append at least one configuration name to start:`);
+  // show all extensions
+  Object.entries(extensions).forEach(([name, ext]) => usage(name, ext.description));
+  console.log('You can also specify a number for tabs.');
+
+  // examples
+  console.log(`\n${colors.blue('EXAMPLES')}`);
+  [
+    ['default', 'use default configuration - good place to start!'],
+    ['default 64', 'default configuration with 64 tabs'],
+    ['inject stdout', 'use injection and prints results to stdout'],
+  ].forEach(([cmd, desc]) => usage(cmd, desc));
+
+  // more informations
   console.log(`\nMore information in ${colors.underline('README.md')} file!\n`);
 
   // success as we asked for help
   process.exit(0);
 }
 
-// check if args are binded to invalid extensions
-const invalid = args.filter(arg => !extensions[arg]);
+// check if args are binded to valid extensions or integers (for tabs)
+const invalid = args.filter(arg => !extensions[arg] && !(parseInt(arg, 10) > 0)); // NaN<0 is false
 if (invalid.length > 0) {
-  console.log(`${colors.red('ERROR')} invalid configuration extensions: ${invalid.join(', ')}`);
-  console.log(`Valids are: ${Object.keys(extensions).join(', ')}`);
+  console.log(`\n${colors.red('ERROR')} invalid configuration extensions: ${invalid.join(', ')}`);
+  console.log(`Valids are: ${Object.keys(extensions).map(ext => colors.white(ext)).join(', ')}, or any integer for # tabs\n`);
   // error as it wasn't expected
   process.exit(110);
 }
 
 // make our config
-const config = args.reduce((obj, param) => deepmerge(obj, extensions[param].config), base);
+const config = args.reduce((conf, arg) => {
+  const val = parseInt(arg, 10);
+  return (val > 0)
+    ? { ...conf, tabs: val } // update tabs
+    : deepmerge(conf, extensions[arg].config); // merge extension
+}, base);
 
 module.exports = config;
